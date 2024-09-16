7b5f01f0ba8bc66551d73c3f39c3cebd193812c9
Wolf_Gabriel <kevinlobo1920k@gmail.com>
2024-09-15 21:40:50 -0600

    cambios de posición

--- move_init.asm
+++ move_init.asm
@@ -1,18 +1,35 @@
 section .data
-    ; Definir los nombres a mostrar (terminados con '$' para la función de DOS)
-    nombre1 db 'Kevin$'
-    nombre2 db 'Nicol$'
-    nombre3 db 'Eder$'
+    ; Definir las versiones de cada nombre según las rotaciones
+    ; Nombre KEVIN
+    kevin_original db 'KEVIN$', 0
+    kevin_left db 'N', 0x0D, 0x0A, 'I', 0x0D, 0x0A, 'V', 0x0D, 0x0A, 'E', 0x0D, 0x0A, 'K', 0x0D, 0x0A, '$', 0
+    kevin_right db 'K', 0x0D, 0x0A, 'E', 0x0D, 0x0A, 'V', 0x0D, 0x0A, 'I', 0x0D, 0x0A, 'N', 0x0D, 0x0A, '$', 0
+    kevin_180 db 'NIVEK$', 0
 
-    ; Mensaje inicial
-    msg db 'Presione "d" para mover el texto a la derecha, "a" para moverlo a la izquierda, "w" para moverlo arriba, "s" para moverlo abajo', 0x0D, 0x0A, '$'
-    change_msg db 'Presione "n" para cambiar el nombre', 0x0D, 0x0A, '$'
+    ; Nombre EDER
+    eder_original db 'EDER$', 0
+    eder_left db 'R', 0x0D, 0x0A, 'E', 0x0D, 0x0A, 'D', 0x0D, 0x0A, 'E', 0x0D, 0x0A, '$', 0
+    eder_right db 'E', 0x0D, 0x0A, 'D', 0x0D, 0x0A, 'E', 0x0D, 0x0A, 'R', 0x0D, 0x0A, '$', 0
+    eder_180 db 'REDE$', 0
+
+    ; Nombre NICOL
+    nicol_original db 'NICOL$', 0
+    nicol_left db 'L', 0x0D, 0x0A, 'O', 0x0D, 0x0A, 'C', 0x0D, 0x0A, 'I', 0x0D, 0x0A, 'N', 0x0D, 0x0A, '$', 0
+    nicol_right db 'N', 0x0D, 0x0A, 'I', 0x0D, 0x0A, 'C', 0x0D, 0x0A, 'O', 0x0D, 0x0A, 'L', 0x0D, 0x0A, '$', 0
+    nicol_180 db 'LOCIN$', 0
+
+    ; Mensajes iniciales
+    msg db 'Presione "d" para rotar 90 grados a la izquierda, "a" para rotar 90 grados a la derecha', 0x0D, 0x0A, '$'
+    msg2 db 'Presione "w" para rotar 180 grados, "s" para rotar 180 grados en el otro sentido', 0x0D, 0x0A, '$'
+    msg3 db 'Presione "1" para KEVIN, "2" para EDER, "3" para NICOL', 0x0D, 0x0A, '$'
+    current_rotation_msg db 'Rotacion actual: $'
+    current_name_msg db 'Nombre actual: $'
+    newline db 0x0D, 0x0A, '$'  ; Nueva línea
 
 section .bss
-    x_pos resb 1          ; Posición X del cursor, inicializada en 20
-    y_pos resb 1          ; Posición Y del cursor, inicializada en 10
-    nombre_sel resb 1     ; Selector de nombre, inicializado en 1
-    key resb 1            ; Variable para almacenar la tecla presionada
+    current_rotation resb 1  ; Estado de rotación
+    current_name resb 1      ; Nombre seleccionado
+    key resb 1               ; Variable para la tecla presionada
 
 section .text
     org 0x100  ; Punto de inicio para un programa .COM en modo real de DOS
@@ -23,153 +40,240 @@ start:
     mov al, 0x03
     int 0x10
 
-    ; Mostrar mensajes iniciales
-    mov ah, 0x09
-    mov dx, msg
-    int 0x21
+    ; Inicializar estado de rotación y nombre
+    mov byte [current_rotation], 0  ; Sin rotación
+    mov byte [current_name], 1      ; Nombre seleccionado por defecto: KEVIN
 
-    mov ah, 0x09
-    mov dx, change_msg
-    int 0x21
-
-    ; Inicializar posición del cursor
-    mov byte [x_pos], 20
-    mov byte [y_pos], 10
-    mov byte [nombre_sel], 1
+    call clear_screen
+    call show_instructions
+    call update_display
 
 main_loop:
     ; Leer la tecla presionada
     mov ah, 0x00
     int 0x16            ; Llamada a la interrupción del BIOS para esperar una tecla
-
-    ; Leer la tecla presionada
-    mov ah, 0x01        ; Función de BIOS para leer la tecla
-    int 0x16            ; Llamada a la interrupción del BIOS para leer la tecla
     mov [key], al
 
-    ; Comprobar si la tecla es 'd' (0x64 en código ASCII)
+    ; Comprobar si la tecla es 'd' (rotación a la izquierda)
     cmp al, 'd'
-    je move_right
+    je rotate_left
 
-    ; Comprobar si la tecla es 'a' (0x61 en código ASCII)
+    ; Comprobar si la tecla es 'a' (rotación a la derecha)
     cmp al, 'a'
-    je move_left
+    je rotate_right
 
-    ; Comprobar si la tecla es 'w' (0x77 en código ASCII)
+    ; Comprobar si la tecla es 'w' (rotación 180 grados)
     cmp al, 'w'
-    je move_up
+    je rotate_180
 
-    ; Comprobar si la tecla es 's' (0x73 en código ASCII)
+    ; Comprobar si la tecla es 's' (volver a la posición original)
     cmp al, 's'
-    je move_down
+    je rotate_original
 
-    ; Comprobar si la tecla es 'n' (0x6E en código ASCII) para cambiar el nombre
-    cmp al, 'n'
-    je change_name
+    ; [El resto de las comprobaciones de teclas permanecen iguales]
 
     jmp main_loop       ; Si no es ninguna tecla manejada, regresar al bucle principal
 
-move_right:
-    ; Mover el texto a la derecha
-    inc byte [x_pos]    ; Incrementar la posición X
-    cmp byte [x_pos], 79
-    jg set_pos_x_max    ; Si excede el límite, ajustar a 79
+rotate_left:
+    mov byte [current_rotation], 1 ; Rotación a la izquierda
     jmp update_display
 
-move_left:
-    ; Mover el texto a la izquierda
-    dec byte [x_pos]    ; Decrementar la posición X
-    cmp byte [x_pos], 0
-    jl set_pos_x_min    ; Si es menor que 0, ajustar a 0
+rotate_right:
+    mov byte [current_rotation], 2 ; Rotación a la derecha
     jmp update_display
 
-move_up:
-    ; Mover el texto hacia arriba
-    dec byte [y_pos]    ; Decrementar la posición Y
-    cmp byte [y_pos], 0
-    jl set_pos_y_min    ; Si es menor que 0, ajustar a 0
+rotate_180:
+    mov byte [current_rotation], 3 ; Rotación 180 grados
     jmp update_display
 
-move_down:
-    ; Mover el texto hacia abajo
-    inc byte [y_pos]    ; Incrementar la posición Y
-    cmp byte [y_pos], 24
-    jg set_pos_y_max    ; Si excede el límite, ajustar a 24
+rotate_original:
+    mov byte [current_rotation], 0 ; Volver a la posición original
     jmp update_display
 
-change_name:
-    ; Cambiar el nombre que se muestra
-    mov al, [nombre_sel]
-    inc al              ; Incrementar el selector de nombre
-    cmp al, 3
-    jg set_name_first   ; Si excede el límite, reiniciar al primer nombre
-    mov [nombre_sel], al
+select_kevin:
+    mov byte [current_name], 1
     jmp update_display
 
-set_name_first:
-    mov byte [nombre_sel], 1  ; Reiniciar al primer nombre
+select_eder:
+    mov byte [current_name], 2
     jmp update_display
 
-set_pos_x_max:
-    mov byte [x_pos], 79
-    jmp update_display
-
-set_pos_x_min:
-    mov byte [x_pos], 0
-    jmp update_display
-
-set_pos_y_max:
-    mov byte [y_pos], 24
-    jmp update_display
-
-set_pos_y_min:
-    mov byte [y_pos], 0
+select_nicol:
+    mov byte [current_name], 3
     jmp update_display
 
 update_display:
-    ; Limpiar la pantalla
+    call clear_screen
+    call show_instructions
+    call show_current_state
+    call display_name
+    jmp main_loop
+
+clear_screen:
     mov ah, 0x06        ; Función 0x06 de BIOS: Desplazamiento de pantalla hacia arriba
     mov al, 0            ; Número de líneas a desplazar (0 = limpiar toda la pantalla)
     mov bh, 0x07         ; Atributo de fondo (color blanco sobre negro)
     mov cx, 0            ; Esquina superior izquierda (fila=0, columna=0)
     mov dx, 184Fh        ; Esquina inferior derecha (fila=24, columna=79)
     int 0x10             ; Llamada a la interrupción de BIOS para limpiar la pantalla
+    ret
 
-    ; Mover el cursor a la nueva posición
-    mov ah, 0x02         ; Función de BIOS para mover el cursor
-    mov bh, 0x00         ; Página de pantalla (0 para modo de texto)
-    mov dh, [y_pos]      ; Fila (0-24)
-    mov dl, [x_pos]      ; Columna (0-79)
-    int 0x10             ; Llamada a la interrupción de BIOS
+show_instructions:
+    mov ah, 0x09
+    mov dx, msg
+    int 0x21
+    mov dx, msg2
+    int 0x21
+    mov dx, msg3
+    int 0x21
+    ret
 
-    ; Mostrar el nombre en pantalla
-    mov al, [nombre_sel] ; Seleccionar el nombre a mostrar
+show_current_state:
+    mov ah, 0x09
+    mov dx, current_rotation_msg
+    int 0x21
+    
+    mov al, [current_rotation]
+    add al, '0'
+    mov ah, 0x0E
+    int 0x10
+    
+    mov ah, 0x02
+    mov dl, 0x0D
+    int 0x21
+    mov dl, 0x0A
+    int 0x21
+    
+    mov ah, 0x09
+    mov dx, current_name_msg
+    int 0x21
+    
+    mov al, [current_name]
+    add al, '0'
+    mov ah, 0x0E
+    int 0x10
+    
+    mov ah, 0x02
+    mov dl, 0x0D
+    int 0x21
+    mov dl, 0x0A
+    int 0x21
+    ret
+
+display_name:
+    mov al, [current_name]
     cmp al, 1
-    je show_nombre1
+    je display_kevin
     cmp al, 2
-    je show_nombre2
+    je display_eder
     cmp al, 3
-    je show_nombre3
-    jmp main_loop
+    je display_nicol
+    ret
 
-show_nombre1:
+display_kevin:
+    mov al, [current_rotation]
+    cmp al, 0
+    je show_kevin_original
+    cmp al, 1
+    je show_kevin_left
+    cmp al, 2
+    je show_kevin_right
+    cmp al, 3
+    je show_kevin_180
+    ret
+
+show_kevin_original:
     mov ah, 0x09
-    mov dx, nombre1
+    mov dx, kevin_original
     int 0x21
-    jmp main_loop
+    ret
 
-show_nombre2:
+show_kevin_left:
     mov ah, 0x09
-    mov dx, nombre2
+    mov dx, kevin_left
     int 0x21
-    jmp main_loop
+    ret
 
-show_nombre3:
+show_kevin_right:
     mov ah, 0x09
-    mov dx, nombre3
+    mov dx, kevin_right
     int 0x21
-    jmp main_loop
+    ret
 
-    ; Salir del programa
-    mov ax, 0x4C00
-    int 0x21            ; Llamada a la interrupción de DOS para salir
+show_kevin_180:
+    mov ah, 0x09
+    mov dx, kevin_180
+    int 0x21
+    ret
+
+display_eder:
+    mov al, [current_rotation]
+    cmp al, 0
+    je show_eder_original
+    cmp al, 1
+    je show_eder_left
+    cmp al, 2
+    je show_eder_right
+    cmp al, 3
+    je show_eder_180
+    ret
+
+show_eder_original:
+    mov ah, 0x09
+    mov dx, eder_original
+    int 0x21
+    ret
+
+show_eder_left:
+    mov ah, 0x09
+    mov dx, eder_left
+    int 0x21
+    ret
+
+show_eder_right:
+    mov ah, 0x09
+    mov dx, eder_right
+    int 0x21
+    ret
+
+show_eder_180:
+    mov ah, 0x09
+    mov dx, eder_180
+    int 0x21
+    ret
+
+display_nicol:
+    mov al, [current_rotation]
+    cmp al, 0
+    je show_nicol_original
+    cmp al, 1
+    je show_nicol_left
+    cmp al, 2
+    je show_nicol_right
+    cmp al, 3
+    je show_nicol_180
+    ret
+
+show_nicol_original:
+    mov ah, 0x09
+    mov dx, nicol_original
+    int 0x21
+    ret
+
+show_nicol_left:
+    mov ah, 0x09
+    mov dx, nicol_left
+    int 0x21
+    ret
+
+show_nicol_right:
+    mov ah, 0x09
+    mov dx, nicol_right
+    int 0x21
+    ret
+
+show_nicol_180:
+    mov ah, 0x09
+    mov dx, nicol_180
+    int 0x21
+    ret
